96a8e5e23391a70cdde06f3711900584f3fcc84e
Robin <torresrobin45@gmail.com>
2020-10-18 09:00:08 +0200

    exercice React 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,56 @@
 import React from 'react';
 import './Contact.css';
+import PropTypes from 'prop-types';
 
-function Contact(){
-    return (
 
-        <figure className="Contact">
-        <img className="avatar"
-        src="https://randomuser.me/api/portraits/men/49.jpg"
-        alt="Nelson Watson"/>
-        <div className="status">
-        <h4 className="name">Nelson Watson</h4>
-        <div className="status-online"></div>
-        <p className="status-text">Online</p>
-        </div>
-        </figure>
-    );
+
+
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={online : props.online}
+    }
+
+
+    Status =() => {
+        const newStatus = !this.state.online;
+        this.setState({ online : newStatus });
+    }
+
+    render(){
+
+        const{avatar}=this.props;
+        const{name}=this.props;
+        const{online}=this.state;
+
+    
+        return(
+
+            <div className='Contact'>
+            <img className='avatar' src={avatar} alt={name}/>
+            <div>
+                <div className='name'>{name}</div>
+                <div className='status'>
+                    <div className={online?'status-online' : 'status-offline'} onClick={()=>this.Status()}></div>
+                    <div className='status-text' onClick={()=>this.Status()}>{online ? 'online' : 'offline'}</div>
+                </div>
+            </div>
+            </div>
+
+        )
+    }
 }
+
+
+
+    Contact.propTypes = {
+
+        name : PropTypes.string,
+        avatar : PropTypes.string,
+        online : PropTypes.bool,
+        
+        
+
+    }
 
 export default Contact;
